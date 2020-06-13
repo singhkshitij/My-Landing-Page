f238a09948b42e9ee1b03f796b88e2141b4f7a02
kshitij.singh <kshitij.singh@go-jek.com>
2020-06-13 21:10:38 +0530

    Updates

---
+++ precache-manifest.80269f058246280502213e60d93978d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fc97112344a900fa57c",
+    "url": "/My-Landing-Page/static/css/main.1ec917e4.chunk.css"
+  },
+  {
+    "revision": "7fc97112344a900fa57c",
+    "url": "/My-Landing-Page/static/js/main.31d22870.chunk.js"
+  },
+  {
+    "revision": "0b7b230b0bdb4cc57608",
+    "url": "/My-Landing-Page/static/js/runtime~main.4f5ad23c.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/My-Landing-Page/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "cc6c6f99db937b51959ecad26414c19a",
+    "url": "/My-Landing-Page/index.html"
+  }
+];
