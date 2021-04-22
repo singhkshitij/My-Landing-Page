fa226b4e7dcdfcda388abe80e63bf4842c5d2644
avinashclassy <avinashclassy@gmail.com>
2021-04-22 11:57:57 +0530

    Updates

---
+++ precache-manifest.615a4aafbddbc030246019ccf87a7d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e384c18784a93c7b0244",
+    "url": "/static/css/main.ef7396dc.chunk.css"
+  },
+  {
+    "revision": "e384c18784a93c7b0244",
+    "url": "/static/js/main.24c419da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d70e9571cbe106007dc6",
+    "url": "/static/js/2.5ebde9bc.chunk.js"
+  },
+  {
+    "revision": "ecd695b5094c5664af328301244446ae",
+    "url": "/index.html"
+  }
+];
